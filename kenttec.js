9a74875c20c6846411d5e89929faf1b0781d95e5
Stephanie Nelima <35595163+Stenewa@users.noreply.github.com>
2022-10-06 12:29:20 +0300

    Add files via upload
    late for the upload but grateful all the same.
    An unpublished front end project I did while on attachment in KENTECC, a parastatal in Kenya.

--- kenttec.js
+++ kenttec.js
@@ -1,11 +1,20 @@
 const hamburger=document.querySelector(".hamburger");
 const navList=document.querySelector(".navlist");
+const hoverList=document.querySelector(".hover_list");
+const navLink=document.querySelector("nav-link");
+const searchButton=document.querySelector("fa-magnifying-glass");
 hamburger.addEventListener("click",()=>{
 	hamburger.classList.toggle("active");
 	navList.classList.toggle("active");
 })
-document.querySelectorAll(".nav-link").forEach(n =>n.addEventListener("click",() =>{
+document.querySelectorAll(".navitem").forEach(n => n.addEventListener("click",() =>{
 	hamburger.classList.remove("active");
 	navList.classList.remove("active");
 
 }))
+
+navLink.addEventListener("click",()=>{
+	hoverList.classList.toggle("active");
+	
+})
+
